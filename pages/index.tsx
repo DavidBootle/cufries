174aaaee87222425e13bee85197d17ef3345fdd1
David Bootle <davidtbootle@gmail.com>
2023-02-04 18:27:54 -0500

    Updated page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import LogoHeader from '@/components/LogoHeader/LogoHeader';
 import React, { useState } from 'react';
 import FrySection from '@/components/FrySection/FrySection';
+import Head from 'next/head';
 
 export function Landing() {
 
     return (
+        <>
+        <Head>
+            <title>CU Fries</title>
+        </Head>
         <div>
             <LogoHeader />
             <FrySection />
         </div>
+        </>
     )
 }
 
