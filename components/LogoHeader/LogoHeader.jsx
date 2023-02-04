ceb8627ff1e200ec2c693d7b327a9027cef8dfed
David Bootle <davidtbootle@gmail.com>
2023-02-04 16:34:51 -0500

    Finished header

--- components/LogoHeader/LogoHeader.jsx
+++ components/LogoHeader/LogoHeader.jsx
@@ -4,12 +4,11 @@ import Image from 'next/image';
 
 const LogoHeader = () => {
 
-
     return (
-        <div className={'flex-col'}>
-            <Image alt="CU Fries logo" src="/CUFries.svg" width={100} height={100} />
-            <h2>CU Fries</h2>
-            <h5>Find your favorite fries in Clemson dining halls.</h5>
+        <div className={'flex-col ' + styles.header}>
+            <Image alt="CU Fries logo" src="/CUFries.svg" className={styles.logo} width={200} height={200}/>
+            <h2 className={styles.headerTitle}>CU Fries</h2>
+            <h5 className={styles.headerSubtitle}>Find your favorite fries in Clemson dining halls.</h5>
         </div>
     )
 }
