e5b304ec81df6952eada3fe5e288784f0c99c28e
David Bootle <davidtbootle@gmail.com>
2023-02-05 09:20:55 -0500

    Undo changes from earlier

--- pages/settings.jsx
+++ pages/settings.jsx
@@ -1,38 +1,41 @@
 import LogoHeader from '@/components/LogoHeader/LogoHeader';
 import React, { useState } from 'react';
+import FrySection from '@/components/FrySection/FrySection';
 import axios from 'axios';
 import Head from 'next/head';
 import toast from "react-hot-toast";
 import styles from './settings.module.css';
-const fs = require('fs/promises');
-
-import dynamic from 'next/dynamic'
-const FrySection = dynamic(() => import('@/components/FrySection/FrySection'), {
-    ssr: false
-})
-
-export async function getServerSideProps(context) {
-    let data = await fs.readFile("/tmp/all_food.json", "utf-8").catch((err) => console.log(err));
-    data = JSON.parse(data);
-    let tmp = data.items;
-    console.log(tmp);
-    tmp.sort((a, b) => {
-        if (a.name > b.name) return 1;
-        if (a.name < b.name) return -1;
-        if (a.name == b.name) return 0;
-    })
-
-    return {
-        props: {fries: tmp}, // will be passed to the page component as props
-    }
-}
 
 export default class Landing extends React.Component {
     constructor(props) {
         super(props);
     
-        this.state = { fries: this.props.fries, loading: false };
+        this.state = { fries: [], loading: true };
       }
+    
+    componentDidMount() {
+    // fetch data from menu-day
+        axios.get("/api/all-food", { timeout: 30000 })
+        .then((response) => {
+            
+            if (response.data && response.data.items) {
+                let tmp = response.data.items;
+                tmp.sort((a, b) => {
+                    if (a.name > b.name) return 1;
+                    if (a.name < b.name) return -1;
+                    if (a.name == b.name) return 0;
+                })
+                this.setState({
+                    fries: response.data.items,
+                    loading: false
+                })
+            }
+        }) 
+        .catch((err) => {
+            toast.error(`Failed to fetch with ${err}`);
+            this.setState({ loading: false} )
+        });
+    }
 
     render() {
         return (
