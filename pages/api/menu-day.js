aa1f4d6c92ac0bf4840f8ff1aa2f871fc9c709cf
David Bootle <davidtbootle@gmail.com>
2023-02-05 09:32:38 -0500

    Day menu now works properly

--- pages/api/menu-day.js
+++ pages/api/menu-day.js
@@ -5,6 +5,17 @@ import internal from "stream";
 const fs = require('fs/promises');
 const fssync = require('fs');
 
+function checkFileExistsSync(filepath){
+    let flag = true;
+    try{
+      fssync.accessSync(filepath, fs.constants.F_OK);
+    }catch(e){
+      flag = false;
+      console.log(e);
+    }
+    return flag;
+}
+
 async function getFromSource(date_obj) {
     let [menu, err] = await get_day_menu(date_obj);
     menu.date = new Date().getDay();
@@ -16,16 +27,16 @@ async function getFromSource(date_obj) {
 export default async function handler(req, res) {
     const { date } = "";
     const date_obj = new Date(date);
-    let fileExists = await fssync.exists(".day_menu.json");
+    let fileExists = checkFileExistsSync(".day_menu.json");
     if (!fileExists) {
         let menu = await getFromSource(date_obj);
         res.status(200).json(menu);
     } else {
-        let menu = await fs.readFile(".day_menu.json");
+        let menu = JSON.parse(await fs.readFile(".day_menu.json"));
         if (menu.date != new Date().getDay()) {
             menu = await getFromSource();
         }
-        res.status(200).json(JSON.parse(menu));
+        res.status(200).json(menu);
     }
 
 }
