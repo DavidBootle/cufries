2f3861947f97f20cee8baa0611790f16b19d7d2f
David Bootle <davidtbootle@gmail.com>
2023-02-04 19:04:30 -0500

    Added responsive elements

--- components/LogoHeader/LogoHeader.jsx
+++ components/LogoHeader/LogoHeader.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const LogoHeader = () => {
 
     return (
-        <div className={'flex-col ' + styles.header}>
+        <div className={styles.header}>
             <Image alt="CU Fries logo" src="/CUFries.svg" className={styles.logo} width={100} height={100}/>
             <h2 className={styles.headerTitle}>CU Fries</h2>
             <h5 className={styles.headerSubtitle}>Find your favorite fries in Clemson dining halls.</h5>
