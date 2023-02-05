64a7481adce4dd6b7a91ebca8d9a898fb1edd1d5
David Bootle <davidtbootle@gmail.com>
2023-02-05 03:55:26 -0500

    Added cron jobs

--- pages/api/all-food.ts
+++ pages/api/all-food.ts
@@ -1,6 +1,14 @@
 import { get_all_food, get_day_menu } from "@/util/scraper";
 import { AllFood, DayMenu } from "@/util/types";
 import { NextApiRequest, NextApiResponse } from "next";
+var cron = require('node-cron');
+const fs = require('fs/promises');
+
+// run every day at midnight
+cron.schedule('0 0 * * *', async () => {
+    saveAllFood();
+});
+
 
 export default async function handler(
     req: NextApiRequest,
@@ -14,3 +22,9 @@ export default async function handler(
 
     res.status(200).json(menu);
 }
+
+async function saveAllFood() {
+    let [menu, err] = await get_all_food();
+    await fs.writeFile("/tmp/all_food.json", JSON.stringify(menu));
+}
+saveAllFood();
--- pages/api/menu-day.ts
+++ pages/api/menu-day.ts
@@ -3,6 +3,14 @@ import { get_day_menu } from "@/util/scraper";
 import { DayMenu } from "@/util/types";
 import type { NextApiRequest, NextApiResponse } from "next";
 import internal from "stream";
+var cron = require('node-cron');
+const fs = require('fs/promises');
+
+// run every day at midnight
+cron.schedule('0 0 * * *', async () => {
+    saveDayMenu();
+});
+
 
 export default async function handler(
     req: NextApiRequest,
@@ -18,3 +26,11 @@ export default async function handler(
 
     res.status(200).json(menu);
 }
+
+// run when the server starts
+async function saveDayMenu() {
+    const date_obj = new Date();
+    let [menu, err] = await get_day_menu(date_obj);
+    await fs.writeFile("/tmp/day_menu.json", JSON.stringify(menu));
+}
+saveDayMenu();
