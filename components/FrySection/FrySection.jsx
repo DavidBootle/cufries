8046806b0693289b505f2650a1c8193da0b1ef4b
David Bootle <davidtbootle@gmail.com>
2023-02-05 00:14:42 -0500

    Delete unused comment

--- components/FrySection/FrySection.jsx
+++ components/FrySection/FrySection.jsx
@@ -2,27 +2,6 @@ import React, { useEffect, useState } from "react";
 import FryCard from "@/components/FryCard/FryCard";
 import styles from "./FrySection.module.css";
 
-/*
-const fries = [
-  {
-    name: "Homestyle French Fries",
-    description: "This is a french fry.",
-  },
-  {
-    name: "Crinkle Cut French Fries",
-    description: "This is a french fry.",
-  },
-  {
-    name: "Shoestring French Fries",
-    description: "This is a french fry.",
-  },
-  {
-    name: "Sweet Potato French Fries",
-    description: "This is a french fry.",
-  },
-];
-*/
-
 
 // PROPS
 // fries - list of all items
